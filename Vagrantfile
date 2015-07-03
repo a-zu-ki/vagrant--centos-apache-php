2466442c3ebc6352bb64bc7df16c24b1669205aa
a-zu-ki <m.nakamura1007@gmail.com>
2015-07-03 15:10:56 +0900

    https://github.com/a-zu-ki/vagrant--centos-apache-base copy

---
+++ Vagrantfile
@@ -0,0 +1,5 @@
+Vagrant.configure(2) do |config|
+  config.vm.box = "chef/centos-6.5"
+  config.vm.network "private_network", ip: "192.168.33.10"
+  config.vm.provision :shell, :path => "provision.sh"
+end
---
+++ provision.sh
@@ -0,0 +1,12 @@
+sudo yum -y install httpd
+sudo mv /etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd.conf.bak 
+sudo cp /vagrant/resources/httpd.conf /etc/httpd/conf/
+sudo cp /vagrant/resources/deflate.conf /etc/httpd/conf.d/
+sudo mv /etc/httpd/conf.d/welcome.conf /etc/httpd/conf.d/welcome.conf.bak
+sudo chown -R vagrant /var/www/html
+sudo ln -s /usr/bin/perl /usr/local/bin/perl
+sudo rm -rf /var/www/html
+sudo ln -fs /vagrant/html /var/www/html
+sudo /etc/rc.d/init.d/httpd start
+sudo chkconfig httpd on
+
