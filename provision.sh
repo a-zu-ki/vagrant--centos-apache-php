f8947cf1a806b93e27378ef22f42e72948f8f1a9
a-zu-ki <m.nakamura1007@gmail.com>
2015-07-03 15:44:31 +0900

    php 5.6.xx インストール

--- provision.sh
+++ provision.sh
@@ -7,7 +7,9 @@ sudo chown -R vagrant /var/www/html
 sudo ln -s /usr/bin/perl /usr/local/bin/perl
 sudo rm -rf /var/www/html
 sudo ln -fs /vagrant/html /var/www/html
-sudo yum -y install php php-mbstring php-mysql
+sudo rpm -Uvh http://ftp.iij.ad.jp/pub/linux/fedora/epel/6/x86_64/epel-release-6-8.noarch.rpm
+sudo rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
+sudo yum -y install --enablerepo=remi --enablerepo=remi-php56 php php-opcache php-devel php-mbstring php-mcrypt php-mysqlnd php-phpunit-PHPUnit php-pecl-xdebug php-pecl-xhprof
 sudo /etc/rc.d/init.d/httpd start
 sudo chkconfig httpd on
 
