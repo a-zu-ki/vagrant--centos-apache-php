b5a3e9fd1a89ac83ffcb700e5c2f012238a5f794
a-zu-ki <m.nakamura1007@gmail.com>
2015-07-10 12:41:10 +0900

    Laravel 5.x インストール

--- Vagrantfile
+++ Vagrantfile
@@ -1,5 +1,6 @@
 Vagrant.configure(2) do |config|
   config.vm.box = "chef/centos-6.5"
   config.vm.network "private_network", ip: "192.168.33.10"
+  config.vm.synced_folder ".", "/vagrant", mount_options: ['dmode=777','fmode=755']
   config.vm.provision :shell, :path => "provision.sh"
 end
--- provision.sh
+++ provision.sh
@@ -3,13 +3,42 @@ sudo mv /etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd.conf.bak
 sudo cp /vagrant/resources/httpd.conf /etc/httpd/conf/
 sudo cp /vagrant/resources/deflate.conf /etc/httpd/conf.d/
 sudo mv /etc/httpd/conf.d/welcome.conf /etc/httpd/conf.d/welcome.conf.bak
+sudo chown -R vagrant /var/www
 sudo chown -R vagrant /var/www/html
 sudo ln -s /usr/bin/perl /usr/local/bin/perl
 sudo rm -rf /var/www/html
 sudo ln -fs /vagrant/html /var/www/html
+
+#php5.6
 sudo rpm -Uvh http://ftp.iij.ad.jp/pub/linux/fedora/epel/6/x86_64/epel-release-6-8.noarch.rpm
 sudo rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
 sudo yum -y install --enablerepo=remi --enablerepo=remi-php56 php php-opcache php-devel php-mbstring php-mcrypt php-mysqlnd php-phpunit-PHPUnit php-pecl-xdebug php-pecl-xhprof
+
+#mysql
+sudo yum -y install http://repo.mysql.com/mysql-community-release-el6-5.noarch.rpm
+sudo yum -y install mysql-community-server
+
+#composer
+sudo curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
+export PATH=$PATH:/usr/local/bin
+
+#laravel
+#composer global require "laravel/installer=~1.1"
+#export PATH=$PATH:~/.composer/vendor/bin
+cd /var/www/
+composer create-project laravel/laravel --prefer-dist laravel
+#laravel new laravel
+sudo mv /var/www/laravel/* /vagrant/laravel/
+sudo rm -rf /var/www/laravel/
+sudo ln -fs /vagrant/laravel /var/www/laravel
+
+
+#apache start
 sudo /etc/rc.d/init.d/httpd start
 sudo chkconfig httpd on
+
+#mysql start
+sudo chkconfig mysqld on
+sudo service mysqld restart
 
